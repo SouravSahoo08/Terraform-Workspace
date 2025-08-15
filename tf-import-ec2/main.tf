41d25c1d23281ee795f84995268fbe50ffb509bd
Sourav Sahoo <jackysourav1994@gmail.com>
2025-08-15 22:27:39 +0530

    tf import

---
+++ tf-import-ec2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "main" {
+  ami           = "ami-0de716d6197524dd9"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "tf-test-import-instance"
+  }
+}
